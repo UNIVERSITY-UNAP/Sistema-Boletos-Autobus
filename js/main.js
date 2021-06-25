9bf550d73a855967c59f080f1a314dce108d688b
omms <ommskara@gmail.com>
2021-06-25 11:05:02 -0500

    first-form

--- js/main.js
+++ js/main.js
@@ -30,4 +30,46 @@ $(document).ready(function () {
 	}
 	render_functions_page(linkInicio)
 	render_functions_page(linkContacto)
+	// const a = document.querySelectorAll(".tab-pane > .row > div")
+	// a.forEach(item=>{
+	//   console.log(item)
+	// })
+
+
+
+	//Modyfing responsive styles
+	const $pe = $(".tab-pane > .row > div")
+	$pe.even().addClass("col-md-3")
+	$pe.odd().addClass("col-md-9")
+
+	// $pe.even().addClass("")
+
+	var $sidebar_content_image = $(".tab-pane > .row > .border-right")
+	function renderBorderSideContent() {
+		if ($(window).width() <= 768)
+			$sidebar_content_image.removeClass("border-right border-gray border-primary border-danger border-sucess border-warning")
+		else
+			$sidebar_content_image.addClass("border-right border-gray")
+	}
+	renderBorderSideContent()
+	$(window).resize(() => {
+		renderBorderSideContent()
+	})
+
+	///Modifing validation
+	'use strict'
+	window.addEventListener('load', () => {
+		// Fetch all the forms we want to apply custom Bootstrap validation styles to
+		var forms = document.getElementsByClassName('needs-validation')
+		// Loop over them and prevent submission
+		var validation = Array.prototype.filter.call(forms, form => {
+			form.addEventListener('submit', event => {
+				if (form.checkValidity() === false) {
+					event.preventDefault()
+					event.stopPropagation()
+				}
+				form.classList.add('was-validated')
+			}, false)
+		})
+	}, false)
 })
